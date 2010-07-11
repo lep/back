e0dffa63072f694141a3de161832d993fede61b5
lep <lep@next.(none)>
2010-07-11 16:53:25 +0200

    base.rb fully (?) working.

--- base.rb
+++ base.rb
@@ -6,34 +6,92 @@ require 'find'
 
 wd2name={}
 
+deleted_files=[]
+deleted_dirs=[]
+created_files=[]
+created_dirs=[]
+modified_files=[]
+
+all_files_i=[]
+all_files_b=[]
+
+queue = []
+cookie_hash={}
+
+Thread.new do
+	while true
+		sleep 30
+	#puts "Deleted files #{deleted_files}"
+	#puts "Deleted dirs #{deleted_dirs}"
+	#puts "Created files #{created_files}"
+	#puts "Created dirs #{created_dirs}"
+	#puts "Modified files #{modified_dirs}"
+	#puts "All files from a #{all_files_i}"
+	#puts "All files in backup #{all_files_b}"
+		puts queue
+		puts "-----------------------------"
+		queue.clear
+	end
+end
+
 dir_watcher=Inotify.new
 
 dir_thread=Thread.new do
 	dir_watcher.each_event do |ev|
-		if ev.mask & Inotify::CREATE>0 and ev.mask & Inotify::ISDIR>0
-			puts "Created"
-			path=File.join(wd2name[ev.wd], ev.name)
-			puts path
-			puts ev.wd
-			puts "--------------"
-			wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
-			wd2name[wd]=path
-		elsif ev.mask & Inotify::DELETE>0# and ev.mask & Inotify::ISDIR>0
-			puts "Delete #{wd2name[ev.wd]}/#{ev.name}"
+		if ev.name!= nil
+			path=File.join(wd2name[ev.wd], ev.name) #TODO: remove base dir
+		end
+		if ev.mask & Inotify::CREATE>0
+			if ev.mask & Inotify::ISDIR>0
+				wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
+				wd2name[wd]=path
+				created_dirs << path
+				queue << { :create_dir => path }
+			else
+				queue << { :create_file => path }
+				created_files << path 
+			end
+		elsif ev.mask & Inotify::DELETE>0
+			if ev.mask & Inotify::ISDIR >0
+				deleted_dirs << path
+				queue << { :delete_dir => path }
+			else
+				deleted_files << path
+				queue << { :delete_file => path }
+			end
 		elsif ev.mask & Inotify::MODIFY>0 and ev.mask & Inotify::ISDIR == 0
-#			puts "Modified #{ev.name}"
-		elsif ev.mask & Inotify::MOVED_TO>0
-			puts "Moved to"
-			puts ev.name
-			puts ev.wd
-			puts wd2name[ev.wd]
-			puts "-------------"
-#			wd2name[ev.wd]=File.join(wd2name[ev.wd], ev.name)
-			path=File.join(wd2name[ev.wd], ev.name)
-			wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
-			wd2name[wd]=path
-			puts "Moved from"
-#			wd2name.delete_if {|k, _| k==File.join(wd2name[ev.wd], ev.name)}
+			modified_files << path
+			queue << { :modify_file => path }
+		elsif ev.mask & Inotify::MOVED_FROM >0
+			cookie_hash[ev.cookie]=ev.cookie
+			if ev.mask & Inotify::ISDIR >0
+				deleted_dirs << path
+				queue << { :delete_dir => path, :cookie => ev.cookie }
+			else
+				deleted_files << path
+				queue << { :delete_file => path, :cookie => ev.cookie }
+			end
+		elsif ev.mask & Inotify::MOVED_TO >0
+			moved_from = queue.select { |v| v[:cookie] == ev.cookie }.first
+			queue.delete moved_from
+			if ev.mask & Inotify::ISDIR >0
+				wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
+				wd2name[wd]=path
+				created_dirs << path
+				
+				if cookie_hash[ev.cookie] == ev.cookie
+					queue << { :move_dir => path, :from =>moved_from[:delete_dir] }
+				else
+					queue << { :copy_dir => path }
+				end
+			else
+				created_files << path
+				if cookie_hash[ev.cookie] == ev.cookie
+					queue << { :move_file => path, :from =>moved_from[:delete_file] }
+				else
+					queue << { :copy_file => path }
+				end
+			end
 		end
 	end
 end
@@ -45,6 +103,13 @@ Find.find('/home/lep/test-i') do |path|
 		wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
 		wd2name[wd]=path
 	end
+	all_files_i << path
+
 end
+
+Find.find('/home/lep/test-b') do |path|
+	all_files_b << path
+end
+
 
 dir_thread.join
