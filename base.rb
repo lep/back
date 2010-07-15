c34915ddd83f139e3f6dfd355c5e26d02340af03
lep <lep@next.(none)>
2010-07-15 18:36:51 +0200

    Added 'class.rb'.

--- base.rb
+++ base.rb
@@ -4,30 +4,15 @@ require 'inotify'
 require 'find'
 
 
-wd2name={}
-
-deleted_files=[]
-deleted_dirs=[]
-created_files=[]
-created_dirs=[]
-modified_files=[]
-
-all_files_i=[]
-all_files_b=[]
-
+wd2name = {}
 queue = []
-cookie_hash={}
+cookie_hash = {}
+base_dir = '/home/lep/test-i/'
+backup_dir = '/home/lep/test-b/'
 
 Thread.new do
 	while true
 		sleep 30
-	#puts "Deleted files #{deleted_files}"
-	#puts "Deleted dirs #{deleted_dirs}"
-	#puts "Created files #{created_files}"
-	#puts "Created dirs #{created_dirs}"
-	#puts "Modified files #{modified_dirs}"
-	#puts "All files from a #{all_files_i}"
-	#puts "All files in backup #{all_files_b}"
 		puts queue
 		puts "-----------------------------"
 		queue.clear
@@ -39,37 +24,40 @@ dir_watcher=Inotify.new
 dir_thread=Thread.new do
 	dir_watcher.each_event do |ev|
 		if ev.name!= nil
-			path=File.join(wd2name[ev.wd], ev.name) #TODO: remove base dir
+			path=File.join(wd2name[ev.wd], ev.name)
+			file=path.clone
+			file[0, base_dir.length]=""
 		end
 		if ev.mask & Inotify::CREATE>0
 			if ev.mask & Inotify::ISDIR>0
 				wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
 				wd2name[wd]=path
-				created_dirs << path
-				queue << { :create_dir => path }
+				queue << { :action => :create_dir, :path => file}
 			else
-				queue << { :create_file => path }
-				created_files << path 
+				queue << { :action => :create_file, :path => file }
 			end
 		elsif ev.mask & Inotify::DELETE>0
 			if ev.mask & Inotify::ISDIR >0
-				deleted_dirs << path
-				queue << { :delete_dir => path }
+				queue << { :action => :delete_dir, :path => file }
 			else
-				deleted_files << path
-				queue << { :delete_file => path }
+				queue << { :action => :delete_file, :path => file }
 			end
 		elsif ev.mask & Inotify::MODIFY>0 and ev.mask & Inotify::ISDIR == 0
-			modified_files << path
-			queue << { :modify_file => path }
+			queue << { :action =>:modify_file, :path => file }
 		elsif ev.mask & Inotify::MOVED_FROM >0
 			cookie_hash[ev.cookie]=ev.cookie
 			if ev.mask & Inotify::ISDIR >0
-				deleted_dirs << path
-				queue << { :delete_dir => path, :cookie => ev.cookie }
+				queue << {
+					:action => :delete_dir, 
+					:path => file, 
+					:cookie => ev.cookie 
+				}
 			else
-				deleted_files << path
-				queue << { :delete_file => path, :cookie => ev.cookie }
+				queue << {
+					:action => :delete_file,
+					:path => file,
+					:cookie => ev.cookie 
+				}
 			end
 		elsif ev.mask & Inotify::MOVED_TO >0
 			moved_from = queue.select { |v| v[:cookie] == ev.cookie }.first
@@ -77,38 +65,42 @@ dir_thread=Thread.new do
 			if ev.mask & Inotify::ISDIR >0
 				wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
 				wd2name[wd]=path
-				created_dirs << path
 				
 				if cookie_hash[ev.cookie] == ev.cookie
-					queue << { :move_dir => path, :from =>moved_from[:delete_dir] }
-				else
-					queue << { :copy_dir => path }
+					queue << {
+						:action => :move_dir,
+						:path => file, 
+						:from =>moved_from[:path] 
+					}
+				else #does not trigger. but it should work with create + modify
+					queue << { :action => :copy_dir, :path => file }
 				end
 			else
-				created_files << path
 				if cookie_hash[ev.cookie] == ev.cookie
-					queue << { :move_file => path, :from =>moved_from[:delete_file] }
-				else
-					queue << { :copy_file => path }
+					queue << {
+						:action => :move_file,
+						:path => file, 
+						:from =>moved_from[:delete_file]
+					}
+				else #does not trigger. :/
+					queue << { :action => :copy_file, :path => file }
 				end
 			end
 		end
 	end
 end
 
-Find.find('/home/lep/test-i') do |path|
+Find.find base_dir do |path|
 	if !File.directory? path
 		Find.prune
 	else
 		wd=dir_watcher.add_watch(path, Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY)
 		wd2name[wd]=path
 	end
-	all_files_i << path
 
 end
 
 Find.find('/home/lep/test-b') do |path|
-	all_files_b << path
 end
 
 
---
+++ class.rb
@@ -0,0 +1,117 @@
+#!/usr/bin/ruby
+
+require 'inotify'
+require 'find'
+
+class BackUp
+	@@mask = Inotify::CREATE | Inotify::DELETE | Inotify::MOVE | Inotify::MODIFY
+
+	def initialize base_dir, backup_dir, interval
+		@base_dir	= base_dir
+		@backup_dir	= backup_dir
+		@interval	= interval
+
+		@wd2name	= {}
+		@queue		= [] #TODO: class for writing this to disk
+		@cookie_hash= {}
+
+		@dir_watcher= Inotify.new
+		self.add_base_watches
+
+		@dir_thread	= Thread.new { self.watch }
+		Thread.new { self.backup }
+
+		@dir_thread.join
+	end
+
+	def backup
+		#TODO: lock queue or something
+		@queue.each do |e|
+			#TODO: dispatch each action. can be overwritten for gui
+		end
+	end
+
+	private
+	def add_base_watches
+		Find.find @base_dir do |p|
+			if not File.directory? p
+				Find.prune
+			else
+				wd=@dir_watcher.add_watch(p, @@mask)
+				@wd2name[wd]=path
+			end
+		end
+	end
+
+	def watch
+		@dir_watcher.each_event do |ev|
+			if ev.name != nil
+				path=File.join(@wd2name[ev.wd], ev.name)
+				file=path.clone
+				file[0, @base_dir.length]=""
+			end
+			
+			if ev.mask & Inotify::CREATE >0
+				if ev.mask & Inotify::ISDIR >0
+					wd=dir_watcher.add_watch(path, @@mask)
+					@wd2name[wd]=path
+					@queue << { :action => :create_dir, :path => file }
+				else
+					@queue << { :action => :create_file, :path => file }
+				end
+			elsif ev.mask & Inotify::DELETE >0
+				@queue << {
+					:action => ev.mask & Inotif::ISDIR >0 ?
+						:delete_dir : 
+						:delete_file,
+					:path => file
+				}
+			elsif ev.mask & Inotify::MODIFY >0 and ev.mask & Inotify::ISDIR == 0
+				@queue << { :action => :modify_file, :path => file }
+			elsif ev.mask & Inotify::MOVED_FROM >0
+				@cookie_hash[ev.cookie] = ev.cookie
+				@queue << {
+					:action => ev.mask & Inotif::ISDIR >0 ?
+						:delete_dir :
+						:delete_file,
+					:path => file,
+					:cookie => ev.cookie
+				}
+			elsif ev.mask & Inotify::MOVED_TO >0
+				moved_from = @queue.select { |v| v[:cookie] == ev.cookie }.first
+				@queue.delete moved_from
+
+				if ev.mask & Inotify::ISDIR >0
+					wd=@dir_watcher.add_watch(path, @@mask)
+					@wd2name[wd]=path
+
+					if @cookie_hash[ev.cookie] == ev.cookie
+						@queue << {
+							:action => :move_dir,
+							:path => file,
+							:from => moved_from[:path]
+						}
+					else #does not trigger~
+						queue << { :action => :copy_dir, :path => file }
+					end
+				else
+					 if @cookie_hash[ev.cookie] == ev.cookie
+					 	@queue << {
+							:action => :move_file,
+							:path => file,
+							:from => moved_from[:path]
+						}
+					 else #does not trigger too
+					 	@queue << { :action => :copy_file, :path => file }
+					 end
+				end
+			end
+
+		
+		end
+	end
+
+
+end
+
+
